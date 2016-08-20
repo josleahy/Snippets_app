0d0bc3f562105e17a4970959b1e30c5f8164630c
Joe Leahy <josleahy@gmail.com>
2016-08-20 19:11:33 +0000

    put/get function

---
+++ snippets.py
@@ -0,0 +1,55 @@
+import logging, argparse
+import psycopg2
+
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets")
+logging.debug("Database connection established.")
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
+    return name, snippet
+
+def get(name):
+    logging.error("Retreiving snippet {!r}".format(name,))
+    cursor = connection.cursor()
+    command = "select keyword, message from snippets where keyword = (%s)".format(name)
+    cursor.execute(command, (name,))
+    connection.commit()
+    logging.debug("Snippet retrieved successfully.")
+    return name
+
+def main():
+    logging.info("Constructing Parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    get_parser = subparsers.add_parser("get", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+    get_parser.add_argument("name", help="Name of the snippet")
+    arguments = parser.parse_args()
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+        
+    
+
+
+if __name__ == "__main__":
+    main()
